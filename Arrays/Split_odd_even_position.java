d7c85d009f973eee51863fda3d3cc8f995384b60
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-28 10:07:01 +0530

    Update Split_odd_even_position.java

--- Arrays/Split_odd_even_position.java
+++ Arrays/Split_odd_even_position.java
@@ -4,17 +4,18 @@ public class SplitOddEvenPosition {
 	public static void main(String[] args) {
 		
 		  int[] arr = {1,2,3,4,5,6,7,8,9,10};
-          int acount=0;int bcount=1;
+		  //System.out.println(arr.length); 
+		  int acount=0;int bcount=1;
           for(int i=0;i<arr.length;i++)
           {              	
-        	if(i<arr.length/2)
+        	if(i<arr.length/2)//0,1,2,3,4,
         	{
-        		System.out.println(acount+2);
+        		System.out.println(arr[acount]);
         		acount=acount+2;
         	}
-        	if(i>=arr.length/2)
+        	if(i>=arr.length/2)//5,6,7,8,9
         	{
-        		System.out.println(bcount);
+        		System.out.println(arr[bcount]);
         		bcount=bcount+2;
         	}
           }          
@@ -22,13 +23,13 @@ public class SplitOddEvenPosition {
 }
 
 Result:-
-2
-4
-6
-8
-10
 1
 3
 5
 7
 9
+2
+4
+6
+8
+10
