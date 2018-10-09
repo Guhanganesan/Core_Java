1b445e1842905f030a19ec260ab13d91b9a9831e
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:38:38 +0530

    Create Parameter_passing.java

---
+++ Methods/Parameter_passing.java
@@ -0,0 +1,25 @@
+package Conditional;
+
+public class Methods1 {
+	
+	
+	public static int add(int a, int b)
+	{
+		return a+b; 
+	}
+
+	public static void main(String[] args) {
+	
+		//reeturn value --> Change void to int(return datatype)
+		//Use new variable to store the returned value
+		
+		int result =add(10,20);//passing values to int a, int b ( parameters)
+		System.out.println(result);
+
+	}
+
+}
+
+
+Result:-
+30
