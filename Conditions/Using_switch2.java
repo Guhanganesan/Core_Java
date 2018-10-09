05913c2364bab061cb7c99522f14ef1480ac7a32
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:12:16 +0530

    Create Using_switch2.java

---
+++ Conditions/Using_switch2.java
@@ -0,0 +1,28 @@
+package Conditional;
+
+public class UsingSwitch {
+
+	public static void main(String[] args) {
+		
+		int number=10;
+		
+		switch(number)//passing the number
+		{
+		
+		 case 10:
+			System.out.println(number);
+		 break;
+		 case 20:
+			 System.out.println(number);
+		 default:
+			 System.out.println(number);
+			 
+		}
+
+	}
+
+}
+
+Result:-
+
+10
