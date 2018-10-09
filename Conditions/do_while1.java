da3db18a77f1e36b3a15e0967f279b2cf9aa8b7e
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:24:34 +0530

    Create do_while1.java

---
+++ Conditions/do_while1.java
@@ -0,0 +1,21 @@
+package Conditional;
+
+public class Using_while {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		do
+		{
+			System.out.println(i);
+			
+			i++;
+		}
+		while(i==0);
+	}
+
+}
+
+
+Result:
+0
