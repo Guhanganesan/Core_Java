7b46878e7b5bc53c7dea292e8d3eef5a39a4d131
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:27:47 +0530

    Create Break.java

---
+++ Conditions/Break.java
@@ -0,0 +1,26 @@
+package Conditional;
+
+public class Using_while {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		while(i<5)
+		{
+			i++;
+			if(i==3)
+			{
+				break;
+			}
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
+
+
+Result:-
+1
+2
