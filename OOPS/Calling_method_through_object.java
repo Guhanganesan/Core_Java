6d271c4f3e4eed25790163e8e08e135b59fbaa28
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 15:38:22 +0530

    Create Calling_method_through_object.java

---
+++ OOPS/Calling_method_through_object.java
@@ -0,0 +1,21 @@
+package BasicOOPS;
+
+public class Ganesan {
+
+	public void teach()
+	{
+		System.out.println("He is teaching");
+	}
+	
+	public static void main(String[] args) {
+		
+		Ganesan obj =new Ganesan();
+		        obj.teach();    
+	}
+
+}
+
+
+Result:-
+
+He is teaching
