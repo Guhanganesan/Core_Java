bdb29f17a6f0dd3c302090120d43a6bf1aee5958
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 13:56:30 +0530

    Create Using_if.java

---
+++ Conditions/Using_if.java
@@ -0,0 +1,23 @@
+package Conditional;
+
+public class UsingIf {
+
+	public static void main(String[] args) {
+		
+		int number=20;
+		if(number==100)
+		{
+			System.out.println(number);
+		}
+		if(number==20)
+		{
+			System.out.println(number);
+		}
+		if(number==50)
+		{
+			System.out.println(number);
+		}
+
+	}
+
+}
