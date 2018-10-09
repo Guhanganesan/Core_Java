99948184c2ff59580ecbf8259d6137c3ebe337b7
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:26:58 +0530

    Create Continue.java

---
+++ Conditions/Continue.java
@@ -0,0 +1,29 @@
+package Conditional;
+
+public class Using_while {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		while(i<5)
+		{
+			i++;
+			if(i==3)
+			{
+				continue;
+			}
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
+
+
+Result:-
+
+1
+2
+4
+5
