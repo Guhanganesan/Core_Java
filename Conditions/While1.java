8497b9dfbc50ca175fd69ccbda9ed8799497579a
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:19:08 +0530

    Create While1.java

---
+++ Conditions/While1.java
@@ -0,0 +1,19 @@
+package Conditional;
+
+public class Using_while {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		while(i<5)
+		{
+			System.out.println(i);
+			break;
+		}
+
+	}
+
+}
+
+Result:-
+0
