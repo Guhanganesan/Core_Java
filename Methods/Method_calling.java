bc67edc98de2ccd869422ddcac4ebcef27a1b991
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:32:09 +0530

    Create Method_calling.java

---
+++ Methods/Method_calling.java
@@ -0,0 +1,24 @@
+package Conditional;
+
+public class Methods1 {
+	
+	static int a=10, b=20;
+	public static void add()
+	{
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+	
+		//Method calling
+		
+		add();
+
+	}
+
+}
+
+
+Result:-
+
+30
