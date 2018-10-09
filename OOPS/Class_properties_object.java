9b432603ec0ef8c0952415974e9d614eab0ca97d
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 15:35:27 +0530

    Create Class_properties_object.java

---
+++ OOPS/Class_properties_object.java
@@ -0,0 +1,17 @@
+package BasicOOPS;
+
+public class Ganesan {
+
+	String name;
+	int age;
+	
+	public static void main(String[] args) {
+		
+		Ganesan obj =new Ganesan();
+		        obj.name="Ganesan";
+		        obj.age=65;
+		System.out.println(obj.name);//Ganesan
+		System.out.println(obj.age);//65
+	}
+
+}
