f5d2184cb7ec34e7ac6667a785768168180dbeec
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 13:54:17 +0530

    Create Using_if_else.java

---
+++ Conditions/Using_if_else.java
@@ -0,0 +1,24 @@
+package Conditional;
+
+public class UsingIf {
+
+	public static void main(String[] args) {
+		
+		int number=100;
+		if(number==100)// when boolean is true
+		{
+			System.out.println("True");
+		}
+		else // when boolean is false
+		{
+			System.out.println("False");
+		}
+
+	}
+
+}
+
+
+Result:-
+
+True
