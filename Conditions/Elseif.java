2ea69cc92302b59b87e639726078ec5b36357568
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-09 14:08:22 +0530

    Create Elseif.java

---
+++ Conditions/Elseif.java
@@ -0,0 +1,33 @@
+package Conditional;
+
+public class UsingIf {
+
+	public static void main(String[] args) {
+		
+		int number=50;
+		
+		if(number==20)
+		{
+			System.out.println(number);
+		}
+		else if(number==30)
+		{
+			System.out.println(number);
+		}
+		else if(number==40)
+		{
+			System.out.println(number);
+		}
+		else
+		{
+			System.out.println(number);
+		}
+		
+	}
+	
+
+}
+
+Result:-
+
+50
