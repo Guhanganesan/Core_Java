67ef86842c140dbe277b69b6f18bc162098a2530
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-11 10:52:03 +0530

    Create Pattern4.java

---
+++ Loopings/Pattern4.java
@@ -0,0 +1,30 @@
+package looping;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		
+		for(int i=3; i>=0; i--)//3,2,1,0
+		{
+			for(int j=0; j<=i; j++)//{0<=3,1<=3,2<=3,3<=3},
+			{				
+				System.out.print(" ");
+	        }
+			for(int k=3; k>=i; k--)//{3>=3}, {3>=2, 2>=2,},{3>=1,2>=1,1>=1,},
+			{  // {3>=0, 2>=0,},{3>=0,2>=0,1>=0,0>=0}
+				System.out.print("*"); 					
+			}
+			System.out.println();
+	  }
+
+	}
+
+}
+
+
+Result:-
+
+    *
+   **
+  ***
+ ****
