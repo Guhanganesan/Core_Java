2176e3d7fed0c98de8981ec15b19c06f73b30c9b
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-12 10:33:57 +0530

    Create College.java

---
+++ Datastructures/College.java
@@ -0,0 +1,18 @@
+package List1;
+
+public class College implements Univ{
+
+	public static void main(String[] args) {
+		
+		Univ obj =new College();
+		     obj.run();
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
