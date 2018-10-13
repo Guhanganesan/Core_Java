7cb81c37eacee552821377b7e1c14ca3ae671053
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-13 17:54:43 +0530

    Create IntereptedException.java

---
+++ Exception/IntereptedException.java
@@ -0,0 +1,24 @@
+package Exception;
+
+public class IntereptedException {
+
+	public static void main(String[] args) throws InterruptedException  
+	{	
+		Thread.sleep(2000); 
+        System.out.println("Hi dear guhan");
+        Thread.sleep(2000);
+        System.out.println("How r u");
+        Thread.sleep(2000);
+        System.out.println("Where r u");
+        Thread.sleep(2000);
+        System.out.println("Fine");
+     }
+}
+
+
+Result:-
+
+Hi dear guhan
+How r u
+Where r u
+Fine
