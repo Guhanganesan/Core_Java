495b7ac68ff0b6c15bc3baa12217d4927eb51ee2
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-11-06 19:50:43 +0530

    Create PrintAlphabets.java

---
+++ Strings/PrintAlphabets.java
@@ -0,0 +1,42 @@
+package MyString;
+
+public class PrintAlphabets {
+
+	public static void main(String[] args) {
+		char chr;
+		for(chr='a';chr<='z';chr++)
+		{
+			System.out.println(chr);
+		}
+
+	}
+
+}
+
+Result:-
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
