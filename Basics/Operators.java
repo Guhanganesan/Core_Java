183bfae045d640e7061109ffa9f117b0813aafbd
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-12-23 21:48:36 +0530

    Create Operators.java

---
+++ Basics/Operators.java
@@ -0,0 +1,54 @@
+package MyPackage;
+
+public class Test3Operators {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		/*
+		System.out.println("Arithmetic Operators");
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println("Relational Operators");
+		System.out.println(2<3);
+		System.out.println(2>3);
+		System.out.println(2<=3);
+		System.out.println(2>=3);
+		System.out.println(2==3);
+		System.out.println(2!=3); 
+		
+		System.out.println("Logical Operators");
+		System.out.println(2<3 && 3>2);
+		System.out.println(2>3 || 2<=3);
+		System.out.println(!true); 
+		
+		
+		System.out.println("Ternary Operators");
+		String x = (2>3)? "Guhan":"Anbu";
+		System.out.println(x);  
+		
+		System.out.println("Unary Operators");
+		int x=10;
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(++x);
+		
+		System.out.println("Bitwise Operators");
+        System.out.println(10&3);
+        System.out.println(10|3);
+        System.out.println(10^5); 
+        System.out.println(~10);
+        System.out.println(~3); */
+		
+		System.out.println("Left shift and right shift");
+		System.out.println(10<<1);
+		System.out.println(10>>1);
+        		
+		
+	}
+
+}
