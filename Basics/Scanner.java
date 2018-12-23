ab255cad876b4aef3da082b6ed6d2cb65afec390
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-12-23 21:49:20 +0530

    Create Scanner.java

---
+++ Basics/Scanner.java
@@ -0,0 +1,36 @@
+package MyPackage;
+
+import java.util.Scanner;
+
+public class Test1Scanner {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		/*
+		System.out.println("Get String Data");
+		String str = sc.next();//only first word
+		// check: he is my friend
+        System.out.println(str); 
+        
+        System.out.println("Get Int Value");
+        int a = sc.nextInt();
+        System.out.println(a); 
+		
+		System.out.println("Get Byte Value");
+		byte b = sc.nextByte();
+		System.out.println(b); 
+		
+		
+		System.out.println("Enter Sentence");
+		String str = sc.nextLine(); // for sentence
+		System.out.println(str);  
+		
+		
+		System.out.println("Enter Character");
+		char c = sc.next().charAt(0);
+		System.out.println(c);  */
+				
+		
+	}
+
+}
