98c2a110991d905cbf37beb003fb33df5ba55abd
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-12-26 14:57:31 +0530

    Create AreaOfCricle.java

---
+++ Basics/AreaOfCricle.java
@@ -0,0 +1,33 @@
+package MyPackage;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// Area = pi * radius * radius;
+		// Radius -> Variable
+		// Pi -> Constant
+		
+		final double Pi=3.14;
+		
+		double Area;
+		
+		double Radius=3.5;
+		
+		Area = Pi * Radius * Radius;
+		System.out.println(Area);
+		
+        //E=mc2
+		
+		long Energy, mass;
+		final long c = 300000000;
+		mass=1000000;
+		Energy = mass*c*c;
+		System.out.println(Energy);
+		
+	}
+
+}
+
+Result:-
+38.465
+-1664335628902334464
