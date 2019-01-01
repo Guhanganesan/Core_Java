d5172e823378fcec77a19e70735a9370f8e40e3f
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-01-01 14:22:12 +0530

    Update and rename Pattern7.java to Pattern8.java

--- Loopings/Pattern8.java
+++ Loopings/Pattern8.java
@@ -2,7 +2,7 @@ package PatternPrograms;
 
 import java.util.Scanner;
 
-public class Pattern7 {
+public class Pattern8 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
