3c1c760ebc8c1e17adeed799cf91bcfc9026bc3c
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-02-05 13:17:51 +0530

    Create Student.java

---
+++ OOPS/Student.java
@@ -0,0 +1,16 @@
+package OOPs;
+
+public class Student {
+
+	int rank;
+	int id;
+	void setValues(int x,int y)
+	{
+		rank=x;
+		id=y;
+	}
+    void getValues()
+    {
+    	System.out.println(rank+" "+id);
+    }
+}
