1e0bce6065aa78ece8a97562d713ebf185d60109
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-02-05 15:02:50 +0530

    Create Calc1.java

---
+++ OOPS/Interfaces/Calc1.java
@@ -0,0 +1,30 @@
+package MyInterface;
+
+public class Calc1 implements Master{
+
+	public static void main(String[] args) {
+		
+		Calc1 obj = new Calc1();
+              obj.test();
+              Master.run();
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mul() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
