d0688af252d2d2ad6b20a07d7cf8e960f9d4eb4f
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-02-05 13:18:42 +0530

    Create StudentDemo.java

---
+++ OOPS/StudentDemo.java
@@ -0,0 +1,15 @@
+package OOPs;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		
+		Student obj = new Student();
+		        obj.setValues(10, 10001);
+                obj.getValues();
+	}
+
+}
+
+Result:-
+10 10001
