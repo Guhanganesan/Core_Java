49d9d934f6dcb8c8f32dc61eafdd9bf4db169a6f
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-02-05 14:49:16 +0530

    Create Master.java

---
+++ OOPS/Abstraction/Master.java
@@ -0,0 +1,13 @@
+package MyAbstract;
+
+public abstract class Master {
+
+	abstract void add();
+	abstract void sub();
+	abstract void mul();
+	void test()
+	{
+		System.out.println("Hi");
+	}
+	
+}
