588bfe21e7f0acf1e1b8a4e9d882bd0ad58915e9
Guhan Ganesan <bharadhiguhan@ymail.com>
2020-05-06 20:10:37 +0530

    Update HashMap1.java

--- Collections/HashMap1.java
+++ Collections/HashMap1.java
@@ -5,44 +5,54 @@ public class HashMap1 {
 	public static void main(String[] args) {
 		
 		Map<Integer,String> data = new HashMap<Integer,String>();
-                data.put(1,"Guhan");
-                data.put(2, "Anbu");
-                data.put(2, "Rajesh");//duplicate key 
-                data.put(3,"Ram");
-                data.put(null, "Jeha");// null is allowed
-                            
-                System.out.println(data);//duplicate key will not allow
-              
-                System.out.println("--- get value by key-----");    
-                System.out.println(data.get(3));
-                
-                System.out.println("--- Remove by key -----");
-                data.remove(null);
-                System.out.println(data);
-                
-                System.out.println("--- Replace values ---");
-                data.replace(3,"Kumar");
-                System.out.println(data);
-                
-                
-                System.out.println("--- print key and values---");
+        data.put(1,"Guhan");
+        data.put(2, "Anbu");
+        data.put(2, "Rajesh");//duplicate key 
+        data.put(3,"Ram");
+        data.put(null, "Jeha");// null is allowed
+          
+        System.out.println(data);//duplicate key will not allow
+      
+        System.out.println("--- get value by key-----");    
+        System.out.println(data.get(3));
+        
+        System.out.println("--- Remove by key -----");
+        data.remove(null);
+        System.out.println(data);
+        
+        System.out.println("--- Replace values ---");
+        data.replace(3,"Kumar");
+        System.out.println(data);
+        
+        
+        System.out.println("--- print key and values---");
 
-                //Traversing the map
-                
-                Set set = data.entrySet();
-                
-                Iterator itr = set.iterator();
-                
-                while(itr.hasNext())
-                {
-                System.out.println(itr.next());	
-                }
+        //Traversing the map
+        
+        Set set = data.entrySet();//list.iterator()
+        
+        Iterator itr = set.iterator();
+        
+        while(itr.hasNext())
+        {
+        System.out.println(itr.next());	
+        }
+        
+        System.out.println(data.containsKey(null));
+        System.out.println(data.containsValue("Guhan"));
+        Set<Integer> ks = data.keySet();
+        for(Integer i:ks)
+        {
+        	System.out.println(i);
+        }
+        
+        data.computeIfPresent(1, (key, val) -> val + "Ganesan"); 
+        System.out.println(data);
      }
 
 }
 
 Result:-
-
 {null=Jeha, 1=Guhan, 2=Rajesh, 3=Ram}
 --- get value by key-----
 Ram
@@ -54,3 +64,9 @@ Ram
 1=Guhan
 2=Rajesh
 3=Kumar
+false
+true
+1
+2
+3
+{1=GuhanGanesan, 2=Rajesh, 3=Kumar}
